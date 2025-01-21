31f2eccd408d5ec20a13751388d9590e4ccb5b40
root <moe_aoun@hotmail.com>
2025-01-21 17:39:52 +0000

    _printf.c

--- _printf.c
+++ _printf.c
@@ -2,7 +2,10 @@
 
 /* Function prototypes */
 int print_number(int n);
-int print_binary(unsigned int n);
+int print_unsigned(unsigned int n);
+int print_octal(unsigned int n);
+int print_hex_lower(unsigned int n);
+int print_hex_upper(unsigned int n);
 
 /**
  * _printf - Function that produces output according to a format.
@@ -22,7 +25,9 @@ int _printf(const char *format, ...)
     {
         if (*ptr == '%' && (*(ptr + 1) == 'c' || *(ptr + 1) == 's' ||
                             *(ptr + 1) == '%' || *(ptr + 1) == 'd' ||
-                            *(ptr + 1) == 'i' || *(ptr + 1) == 'b'))
+                            *(ptr + 1) == 'i' || *(ptr + 1) == 'b' ||
+                            *(ptr + 1) == 'u' || *(ptr + 1) == 'o' ||
+                            *(ptr + 1) == 'x' || *(ptr + 1) == 'X'))
         {
             if (*(ptr + 1) == 'c')  /* Handle character */
             {
@@ -62,6 +67,30 @@ int _printf(const char *format, ...)
                 unsigned int num = va_arg(args, unsigned int);
 
                 count += print_binary(num);
+            }
+            else if (*(ptr + 1) == 'u')  /* Handle unsigned integer */
+            {
+                unsigned int num = va_arg(args, unsigned int);
+
+                count += print_unsigned(num);
+            }
+            else if (*(ptr + 1) == 'o')  /* Handle octal */
+            {
+                unsigned int num = va_arg(args, unsigned int);
+
+                count += print_octal(num);
+            }
+            else if (*(ptr + 1) == 'x')  /* Handle lowercase hexadecimal */
+            {
+                unsigned int num = va_arg(args, unsigned int);
+
+                count += print_hex_lower(num);
+            }
+            else if (*(ptr + 1) == 'X')  /* Handle uppercase hexadecimal */
+            {
+                unsigned int num = va_arg(args, unsigned int);
+
+                count += print_hex_upper(num);
             }
 
             ptr++;  /* Skip the next character */
@@ -113,16 +142,14 @@ int print_number(int n)
 }
 
 /**
- * print_binary - Helper function to print an unsigned integer in binary.
- * @n: The unsigned integer to print in binary.
+ * print_unsigned - Helper function to print an unsigned integer.
+ * @n: The unsigned integer to print.
  *
  * Return: The number of characters printed.
  */
-int print_binary(unsigned int n)
+int print_unsigned(unsigned int n)
 {
     int count = 0;
-    char binary[32];
-    int i = 0;
 
     /* Handle the special case of zero */
     if (n == 0)
@@ -131,20 +158,97 @@ int print_binary(unsigned int n)
         return (1);
     }
 
-    /* Convert to binary by dividing by 2 and storing remainders */
-    while (n > 0)
+    if (n / 10 != 0)
     {
-        binary[i] = (n % 2) + '0';
-        n /= 2;
-        i++;
+        count += print_unsigned(n / 10);
     }
 
-    /* Print binary in reverse order */
-    while (i--)
+    write(1, &"0123456789"[n % 10], 1);
+    count++;
+
+    return (count);
+}
+
+/**
+ * print_octal - Helper function to print an unsigned integer in octal.
+ * @n: The unsigned integer to print in octal.
+ *
+ * Return: The number of characters printed.
+ */
+int print_octal(unsigned int n)
+{
+    int count = 0;
+
+    /* Handle the special case of zero */
+    if (n == 0)
     {
-        write(1, &binary[i], 1);
-        count++;
+        write(1, "0", 1);
+        return (1);
     }
+
+    if (n / 8 != 0)
+    {
+        count += print_octal(n / 8);
+    }
+
+    write(1, &"01234567"[n % 8], 1);
+    count++;
+
+    return (count);
+}
+
+/**
+ * print_hex_lower - Helper function to print an unsigned integer in lowercase hexadecimal.
+ * @n: The unsigned integer to print in lowercase hexadecimal.
+ *
+ * Return: The number of characters printed.
+ */
+int print_hex_lower(unsigned int n)
+{
+    int count = 0;
+
+    /* Handle the special case of zero */
+    if (n == 0)
+    {
+        write(1, "0", 1);
+        return (1);
+    }
+
+    if (n / 16 != 0)
+    {
+        count += print_hex_lower(n / 16);
+    }
+
+    write(1, &"0123456789abcdef"[n % 16], 1);
+    count++;
+
+    return (count);
+}
+
+/**
+ * print_hex_upper - Helper function to print an unsigned integer in uppercase hexadecimal.
+ * @n: The unsigned integer to print in uppercase hexadecimal.
+ *
+ * Return: The number of characters printed.
+ */
+int print_hex_upper(unsigned int n)
+{
+    int count = 0;
+
+    /* Handle the special case of zero */
+    if (n == 0)
+    {
+        write(1, "0", 1);
+        return (1);
+    }
+
+    if (n / 16 != 0)
+    {
+        count += print_hex_upper(n / 16);
+    }
+
+    write(1, &"0123456789ABCDEF"[n % 16], 1);
+    count++;
 
     return (count);
 }
