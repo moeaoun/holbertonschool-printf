df12292faad25ca547f164fa0759b5d0c1b4a48d
root <moe_aoun@hotmail.com>
2025-01-21 17:33:28 +0000

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
-/* Function prototype for print_number */
+/* Function prototypes */
 int print_number(int n);
+int print_binary(unsigned int n);
 
 /**
  * _printf - Function that produces output according to a format.
@@ -20,11 +21,13 @@ int _printf(const char *format, ...)
     for (ptr = format; *ptr != '\0'; ptr++)
     {
         if (*ptr == '%' && (*(ptr + 1) == 'c' || *(ptr + 1) == 's' ||
-            *(ptr + 1) == '%' || *(ptr + 1) == 'd' || *(ptr + 1) == 'i'))
+                            *(ptr + 1) == '%' || *(ptr + 1) == 'd' ||
+                            *(ptr + 1) == 'i' || *(ptr + 1) == 'b'))
         {
             if (*(ptr + 1) == 'c')  /* Handle character */
             {
                 char c = va_arg(args, int);
+
                 write(1, &c, 1);
                 count++;
             }
@@ -33,7 +36,9 @@ int _printf(const char *format, ...)
                 char *str = va_arg(args, char *);
 
                 if (str == NULL)
+                {
                     str = "(null)";
+                }
 
                 while (*str)
                 {
@@ -49,7 +54,14 @@ int _printf(const char *format, ...)
             else if (*(ptr + 1) == 'd' || *(ptr + 1) == 'i')  /* Handle integer */
             {
                 int num = va_arg(args, int);
+
                 count += print_number(num);
+            }
+            else if (*(ptr + 1) == 'b')  /* Handle binary */
+            {
+                unsigned int num = va_arg(args, unsigned int);
+
+                count += print_binary(num);
             }
 
             ptr++;  /* Skip the next character */
@@ -93,8 +105,46 @@ int print_number(int n)
     {
         count += print_number(num / 10);
     }
+
     write(1, &"0123456789"[num % 10], 1);
     count++;
+
+    return (count);
+}
+
+/**
+ * print_binary - Helper function to print an unsigned integer in binary.
+ * @n: The unsigned integer to print in binary.
+ *
+ * Return: The number of characters printed.
+ */
+int print_binary(unsigned int n)
+{
+    int count = 0;
+    char binary[32];
+    int i = 0;
+
+    /* Handle the special case of zero */
+    if (n == 0)
+    {
+        write(1, "0", 1);
+        return (1);
+    }
+
+    /* Convert to binary by dividing by 2 and storing remainders */
+    while (n > 0)
+    {
+        binary[i] = (n % 2) + '0';
+        n /= 2;
+        i++;
+    }
+
+    /* Print binary in reverse order */
+    while (i--)
+    {
+        write(1, &binary[i], 1);
+        count++;
+    }
 
     return (count);
 }
