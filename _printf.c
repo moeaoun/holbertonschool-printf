aec5b6734395ec6a1a6ed1bd3dbdbf9c43f3bb89
root <moe_aoun@hotmail.com>
2025-01-21 17:25:10 +0000

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+/* Function prototype for print_number */
+int print_number(int n);
+
 /**
  * _printf - Function that produces output according to a format.
  * @format: The format string containing the directives.
@@ -17,7 +20,7 @@ int _printf(const char *format, ...)
     for (ptr = format; *ptr != '\0'; ptr++)
     {
         if (*ptr == '%' && (*(ptr + 1) == 'c' || *(ptr + 1) == 's' ||
-            *(ptr + 1) == '%'))
+            *(ptr + 1) == '%' || *(ptr + 1) == 'd' || *(ptr + 1) == 'i'))
         {
             if (*(ptr + 1) == 'c')  /* Handle character */
             {
@@ -43,6 +46,11 @@ int _printf(const char *format, ...)
                 write(1, "%", 1);
                 count++;
             }
+            else if (*(ptr + 1) == 'd' || *(ptr + 1) == 'i')  /* Handle integer */
+            {
+                int num = va_arg(args, int);
+                count += print_number(num);
+            }
 
             ptr++;  /* Skip the next character */
         }
@@ -54,6 +62,40 @@ int _printf(const char *format, ...)
     }
 
     va_end(args);
+    return (count);
+}
+
+/**
+ * print_number - Helper function to print an integer.
+ * @n: The integer to print.
+ *
+ * Return: The number of characters printed.
+ */
+int print_number(int n)
+{
+    int count = 0;
+    unsigned int num;
+
+    /* Handle negative numbers */
+    if (n < 0)
+    {
+        write(1, "-", 1);
+        count++;
+        num = -n;
+    }
+    else
+    {
+        num = n;
+    }
+
+    /* Print the digits of the number */
+    if (num / 10 != 0)
+    {
+        count += print_number(num / 10);
+    }
+    write(1, &"0123456789"[num % 10], 1);
+    count++;
+
     return (count);
 }
 
